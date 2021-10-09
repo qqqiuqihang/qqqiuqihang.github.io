21d81df98308fc459836b99c04372d2c864c8d31
qqh <571424332@qq.com>
2021-10-09 13:43:16 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+
+
+# 当发生错误时中止脚本
+
+set -e
+
+
+
+# 构建
+
+npm run build
+
+
+
+# cd 到构建输出的目录下 
+
+cd dist
+
+
+
+git init
+
+git add -A
+
+git commit -m 'deploy'
+
+
+
+# 部署到 https://qqqiuqihang.github.io
+git push -f git@github.com:qqqiuqihang.git gh-pages:gh-pages
+
+
+
+cd -
---
+++ vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  devServer: {
+    open: true,
+    port: 3001,
+  },
+  lintOnSave: false,
+  productionSourceMap: false,
+};

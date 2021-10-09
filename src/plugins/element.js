eee74e1b972d87a3669ce6f6831bcf6d4399f7b5
qqh <571424332@qq.com>
2021-10-09 16:02:51 +0800

    init

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -2,8 +2,6 @@ import { ElButton, locale } from "element-plus";
 import lang from "element-plus/lib/locale/lang/zh-cn";
 import "element-plus/lib/theme-chalk/index.css";
 
-console.log(locale);
-
 export default (app) => {
   locale(lang);
   app.use(ElButton);

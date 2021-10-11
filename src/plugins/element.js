d591349c3d8b2f82c0f21d690b1d53c75d66bc75
qqh <571424332@qq.com>
2021-10-11 11:17:06 +0800

    10.11 11:00

--- src/plugins/element.js
+++
@@ -1,8 +0,0 @@
-import { ElButton, locale } from "element-plus";
-import lang from "element-plus/lib/locale/lang/zh-cn";
-import "element-plus/lib/theme-chalk/index.css";
-
-export default (app) => {
-  locale(lang);
-  app.use(ElButton);
-};
--- vue.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  devServer: {
-    open: true,
-    port: 3001,
-  },
-  lintOnSave: false,
-  productionSourceMap: false,
-};

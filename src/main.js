d22b5bdfefae0d53d7d12a96a7520e9e8b7e25a9
QQH <571424332@qq.com>
2021-12-01 17:54:12 +0800

    12.01 17.54

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import "./mock";
 
 // 导入初始化和全局样式
 import "./assets/css/global.css";
-import "./assets/css/initStyle.css"
+import "./assets/css/initStyle.css";
 // 引入字体图标
 import "./assets/iconfont/iconfont.css";
 

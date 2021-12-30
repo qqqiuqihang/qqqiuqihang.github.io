a391167c7c5d8d23f20078d2606a2515d608eb0d
qqh <571424332@qq.com>
2021-12-30 17:24:10 +0800

    12.30 17.24

--- src/views/Map/iserver.vue
+++ src/views/Map/iserver.vue
@@ -1,24 +1,116 @@
 <template>
   <div class="iserver-container">
     <div class="map_container">
-      <YXMap />
+      <YXMap ref="YXMap" />
+    </div>
+    <div class="search-left">
+      <div class="search_wrap">
+        <el-input
+          v-model="input3"
+          placeholder="Please input"
+          class="input-with-select"
+        >
+          <template #prepend>
+            <el-select
+              v-model="select"
+              placeholder="Select"
+              style="width: 110px"
+            >
+              <el-option label="重点单位" value="1"></el-option>
+              <el-option label="兴趣点" value="2"></el-option>
+            </el-select>
+          </template>
+          <template #append>
+            <el-button @click="searchInput">
+              <span class="iconfont icon-icon-test12"></span>
+            </el-button>
+          </template>
+        </el-input>
+      </div>
+    </div>
+    <div class="search-right">
+      <div class="btn-item" @click="toggleMeasure('distance')">
+        <span
+          class="iconfont icon-celiang2"
+          :class="{ active: active === 'distance' }"
+          title="测量距离"
+        ></span>
+      </div>
+      <div class="btn-item" @click="toggleMeasure('area')">
+        <span
+          class="iconfont icon-shitucemianji"
+          :class="{ active: active === 'area' }"
+          title="测量面积"
+        ></span>
+      </div>
+      <div class="btn-item" @click="toggleMeasure('clear')">
+        <span class="iconfont icon-icon-test1" title="退出测量"></span>
+      </div>
+      <!-- <div
+        class="btn-item"
+        @click="toggleMeasure(item.type)"
+        v-for="(item, index) in toolListsMap"
+        :key="index"
+      >
+        <span
+          class="iconfont"
+          :class="[item.icon, active === item.type ? 'active' : '']"
+          :title="item.title"
+        ></span>
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
 import YXMap from "@/components/Map/YXMap.vue";
+import QueryTask from "@arcgis/core/tasks/QueryTask";
+import Query from "@arcgis/core/tasks/support/Query";
 export default {
   name: "IserverMap",
   data() {
-    return {};
+    return {
+      active: "",
+      toolListsMap: [
+        { type: "distance", icon: "icon-celiang2", title: "测量距离" },
+        { type: "area", icon: "icon-shitucemianji", title: "测量面积" },
+        { type: "clear", icon: "icon-icon-test1", title: "退出测量" },
+      ],
+      select: "1",
+      input3: "",
+    };
   },
   components: { YXMap },
   created() {},
   mounted() {},
-  methods: {},
+  methods: {
+    searchInput() {
+      let queryTask = new QueryTask({
+        url: "http://10.12.6.90:8090/iserver/services/map-mbtiles-ArcTiler141427yxkml/wmts100",
+      });
+      let query = new Query();
+      query.returnGeometry = true;
+      query.outFields = ["*"];
+      query.where = "POP &gt; 1000000"; // Return all cities with a population greater than 1 million
+
+      // When resolved, returns features and graphics that satisfy the query.
+      queryTask.execute(query).then(function (results) {
+        console.log(results.features);
+      });
+
+      // When resolved, returns a count of the features that satisfy the query.
+      queryTask.executeForCount(query).then(function (results) {
+        console.log(results);
+      });
+    },
+    toggleMeasure(type) {
+      this.active = type;
+      this.$refs.YXMap.switchTool(type);
+    },
+  },
 };
 </script>
+
 <style lang="less" scoped>
 .iserver-container {
   width: 100%;
@@ -29,6 +121,32 @@ export default {
     left: 0;
     right: 0;
     bottom: 0;
+  }
+  .search-left {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+  }
+  .search-right {
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    z-index: 5;
+    background-color: #fff;
+    display: flex;
+    .btn-item {
+      border: 1px solid #000;
+      border-collapse: collapse;
+      span {
+        display: inline-block;
+        width: 35px;
+        height: 35px;
+        font-size: 35px;
+      }
+      .active {
+        background-color: #409eff;
+      }
+    }
   }
 }
 </style>
